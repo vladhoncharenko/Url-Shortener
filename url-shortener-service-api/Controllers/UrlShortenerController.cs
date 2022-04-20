b6ac0b874c0c3641d5dbfe44d201de6cb0c90f1a
Vladyslav Honcharenko <vladhoncharenko@gmail.com>
2022-04-20 19:37:11 +0200

    UrlShortenerController was added

---
+++ url-shortener-service-api/Controllers/UrlShortenerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UrlShortenerService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UrlShortenerController : ControllerBase
+    {
+        private readonly ILogger<UrlShortenerController> _logger;
+
+        public UrlShortenerController(ILogger<UrlShortenerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPut("{url}", Name = "CreateAShortLink")]
+        public async Task<ActionResult<string>> Put(string url)
+        {
+            return Ok("https://test.com");
+        }
+
+        [HttpGet("/{url}", Name = "RedirectToTheOriginalLink")]
+        public async Task<ActionResult> Get(string url)
+        {
+            return RedirectPermanent("https://www.google.com");
+        }
+    }
+}
